3904cc6dc2aa62155c425bd887cfefe22b8d915b
John Long <me@johnwlong.com>
2007-10-13 05:18:08 +0000

    fixed init.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 Dependencies.load_paths << "#{config.root_path}/spec/scenarios"
 if config.environment == "test"
-  require 'spec/scenarios'
+  require 'scenarios'
   require 'spec/rails'
-  Dir[File.dirname(__FILE__) + '/lib/spec/scenarios/dsl/extensions/*.rb'].each { |f| require f }
+  Dir[File.dirname(__FILE__) + '/lib/scenarios/dsl/extensions/*.rb'].each { |f| require f }
 end
