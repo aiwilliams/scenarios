5a64de08a2a47d57149180b20c388e5a4bdaf7e0
Adam Williams <adam@thewilliams.ws>
2007-11-11 03:04:17 +0000

    scenarios: made to work with rspec rev 2847

--- lib/scenarios/dsl/extensions.rb
+++ lib/scenarios/dsl/extensions.rb
@@ -1,24 +1,6 @@
 require File.dirname(__FILE__) + "/extensions/behaviour"
 require File.dirname(__FILE__) + "/extensions/example"
 
-Spec::DSL::Example.module_eval do
-  def self.inherited(klass) # :nodoc:
-    super
-    klass.extend Scenarios::DSL::ClassMethods
-  end
-end
-
-# # Some may may include this
-# Spec::DSL::ExampleModule.module_eval do
-#   include Scenarios::DSL::ExampleExtensions
-# end
-
-# And some may extend this
-Spec::DSL::Example.module_eval do
-  include Scenarios::DSL::ExampleExtensions
-end
-
-# While others work under here
 Test::Unit::TestCase.module_eval do
   extend Scenarios::DSL::ClassMethods
   include Scenarios::DSL::ExampleExtensions
