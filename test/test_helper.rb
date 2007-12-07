92df7d6ae990e28e1690ae1b0a9a9b938dd0004d
Adam Williams <adam@thewilliams.ws>
2007-12-07 21:42:42 +0000

    scenarios: removed requirement of test/unit which caused superflous output when using rake load task

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + "/../testing/environment")
 TESTING_ENVIRONMENTS[TESTING_ENVIRONMENT || "RSpec/Rails Trunk"].load(DATABASE_ADAPTER || "mysql")
 require "models"
+require "test/unit"
 require "scenarios"
