9bb300f7ccb50a4da7261e2bc9af30e7c7b13c5b
Adam Williams <adam@thewilliams.ws>
2008-06-19 15:46:45 -0400

    Cleaned up Rakefile, made the default environment be edge rails/rspec.

--- Rakefile
+++ Rakefile
@@ -1,22 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/testing/plugit_descriptor')
 
 require 'rake/rdoctask'
-# require 'rake/testtask'
-# require "spec/rake/spectask"
-
-# desc "Run specs in #{ENV['PLUGIT_ENV']} environment. Change using ENV['PLUGIT_ENV']."
-# Spec::Rake::SpecTask.new :spec do |t|
-#   t.fail_on_error = false
-#   t.spec_files = FileList["spec/*_spec.rb"]
-#   t.verbose = false
-# end
-# 
-# desc "Run tests in environment of ENV['PLUGIT_ENV']"
-# Rake::TestTask.new :test do |t|
-#   t.test_files = FileList["test/**/*_test.rb"]
-#   t.verbose = false
-# end
-# 
 Rake::RDocTask.new(:doc) do |r|
   r.title = "Rails Scenarios Plugin"
   r.main = "README"
@@ -24,5 +8,3 @@ Rake::RDocTask.new(:doc) do |r|
   r.rdoc_files.include("README", "LICENSE", "lib/**/*.rb")
   r.rdoc_dir = "doc"
 end
-# 
-# task :default => [:spec, :test]
--- testing/plugit_descriptor.rb
+++ testing/plugit_descriptor.rb
@@ -25,4 +25,20 @@ Plugit.describe do |scenarios|
       rspec_rails.requires = %w{spec/rails}
     end
   end
+  
+  scenarios.environment :edge, 'Edge versions of Rails and RSpec' do |env|
+    env.library :rails, :export => "git clone git://github.com/rails/rails.git --depth 1" do |rails|
+      rails.before_install { `git pull` }
+      rails.load_paths = %w{/activesupport/lib /activerecord/lib /actionpack/lib}
+      rails.requires = %w{active_support active_record action_controller action_view}
+    end
+    env.library :rspec, :export => "git clone git://github.com/dchelimsky/rspec.git --depth 1" do |rspec|
+      rspec.after_update { `git pull && mkdir -p #{vendor_directory} && ln -sF #{File.expand_path('.')} #{vendor_directory + '/rspec'}` }
+      rspec.requires = %w{spec}
+    end
+    env.library :rspec_rails, :export => "git clone git://github.com/dchelimsky/rspec-rails.git --depth 1" do |rspec_rails|
+      rspec_rails.after_update { `git pull` }
+      rspec_rails.requires = %w{spec/rails}
+    end
+  end
 end
